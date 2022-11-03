5889d74484c55efc3cb3d87120d12d70d98628bb
Gor <gor@aaa.sss>
2022-11-03 06:26:53 +0000

    heat.php

--- session/heat.php
+++ session/heat.php
@@ -4,20 +4,32 @@ table {
 }  
 th {
     border: 1px solid black;
+    text-align: center;
+    padding: 10px;
+    background-color: green;
 }
 td {
     border: 1px solid black;
+    text-align: center;
 }
+tbody>tr:nth-child(odd) {
+    background-color: #c9bdad;
+}
+tbody>tr:nth-child(even) {
+    background-color: #8c705e;
+}
+
+
 </style>
 
 <table>
 <thead>
     <tr>
-        <th>ID</th>
+        <th>№</th>
         <th>Имя</th>
         <th>Логин</th>
         <th>Возраст</th>
-        <th>ID группы</th>
+        <th>Группа</th>
     </tr>
 </thead>
 <tbody>
@@ -26,14 +38,22 @@ td {
 
 
 <?php
-	$host = 'my_db'; // имя хоста
-	$user = 'root';      // имя пользователя
-	$pass = '1234';          // пароль
-	$name = 'users';      // имя базы данных
+	$host = 'my_db';    // имя хоста
+	$user = 'root';     // имя пользователя
+	$pass = '1234';     // пароль
+	$name = 'users';    // имя базы данных
 	
 	$link = mysqli_connect($host, $user, $pass, $name);
-	
-	$query = 'SELECT `ID`, `NAME`, `LOGIN`, `AGE`,`ID_GROUP` FROM `students` ';
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $login = $_POST['login'];
+    $age = $_POST['age'];
+    $id_group = $_POST['id_group'];
+    $query = sprintf("INSERT INTO `students` (`NAME`,`LOGIN`,`AGE`,`ID_GROUP`)VALUES ('%s', '%s', %d, %d)", $name,$login,$age,$id_group);
+    mysqli_query($link, $query) or die(mysqli_error($link));
+}
+
+	$query = 'SELECT s.`ID`,s.`NAME`, s.`LOGIN`, s.`AGE`,g.`NAME` AS `GROUP` FROM `students` AS s JOIN `groops` AS g ON g.ID = s.ID_GROUP;';
 	$result = mysqli_query($link, $query) or die(mysqli_error($link));
 	while ($row = mysqli_fetch_assoc($result)) {
         echo '<tr>';
@@ -41,10 +61,26 @@ td {
         echo '<td>'.$row['NAME'].'</td>';
         echo  '<td>'.$row['LOGIN'].'</td>';
         echo  '<td>'.$row['AGE'].'</td>';
-        echo  '<td>'.$row['ID_GROUP'].'</td>';
+        echo  '<td>'.$row['GROUP'].'</td>';
         echo '</tr>';
 
     }
 ?>
 </tbody>
 </table>
+<br>
+<form action="heat.php" method="POST">
+    <input type="text" name="name" placeholder="Имя"><br>
+    <input type="text" name="login" placeholder="Логин"><br>
+    <input type="text" name="age" placeholder="Возраст"><br>
+    <select name="group">
+    <?php 
+    $query = 'SELECT `ID`, `NAME` FROM `groops`';
+    $result = mysqli_query($link, $query) or die(mysqli_error($link));
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<option>'.$rov['NAME'].'</option>';
+    }
+    ?>
+    </select><br>
+    <input type="submit">
+</form>
